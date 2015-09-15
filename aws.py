f105f6f728c581f8385157a7be7bb483d08ad08f
KeleiAzz <kgong@ncsu.edu>
2015-09-14 20:14:27 -0400

    AWS almost finish

--- aws.py
+++ aws.py
@@ -1,38 +1,72 @@
 import boto.ec2
 import os
 from boto.ec2 import address
+import time
+import boto3
 
-conn = boto.ec2.connect_to_region('us-east-1')
-
-# conn.run_instances(
-#         'ami-d05e75b8',
-#         key_name='mac',
-#         instance_type='t2.micro',
-#         security_groups=['default'],
-#         min_count=3,
-#         max_count=4,
-#         # instance_profile_name = 'test1'
-#     )
-
-status = conn.get_all_instance_status()[0]
-conn.get_all_instances()[2].instances
-
-conn.get_all_reserved_instances()
-
-ip = conn.get_only_instances()[0].ip_address
-
-print conn.get_all_instance_status()[0].state_name
+ec2 = boto3.resource('ec2')
 
 
-def createInventory(key_file):
-    id = conn.get_only_instances()[0].id
-    ip = conn.get_only_instances()[0].ip_address
+def createInstances(num, key='mac', security_group='default'):
+    instances = ec2.create_instances(
+        ImageId='ami-d05e75b8',
+        MinCount=num,
+        MaxCount=num,
+        SecurityGroups=[security_group],
+        InstanceType='t2.micro',
+        KeyName=key
+    )
+    return instances
+
+def createKeyPairs(name, public_key_file):
+    f = open(public_key_file, 'r')
+    key = f.read()
+    f.close()
+    key_pair = ec2.import_key_pair(
+        KeyName=name,
+        PublicKayMaterial=key
+    )
+    return key_pair
+
+def createSecurityGroup(name, description):
+    sg = ec2.create_security_group(
+        GroupName=name,
+        Description=description
+    )
+    sg.authorize_ingress(
+        IpProtocol='-1',
+        CidrIp='0.0.0.0/0'
+    )
+
+
+def createInventory(instances, key_file):
+    # id = conn.get_only_instances()[0].id
+    # ip = conn.get_only_instances()[0].ip_address
     key_file_path = os.path.abspath(key_file)
     user = 'ubuntu'
-    s = '%s ansible_ssh_host=%s ansible_ssh_user=%s ansible_ssh_private_key_file=%s' % (id, ip, user, key_file_path,)
-    f = open('inventory', 'ab')
-    print >> f, s
-    # f.save()
-    f.close()
+    ids = [ins.id for ins in instances]
+    ips = []
+    for id in ids:
+        ips.append(ec2.Instance(id).public_ip_address)
+    while(True):
+        if all(ips):
+            f = open('inventory', 'ab')
+            for i in range(len(ips)):
+                s = '%s ansible_ssh_host=%s ansible_ssh_user=%s ansible_ssh_private_key_file=%s' % (ids[i], ips[i], user, key_file_path,)
+                print >> f, s
+            f.close()
+            break
+        else:
+            print "Waiting for IP address"
+            time.sleep(2)
+            ips = []
+            for id in ids:
+                ips.append(ec2.Instance(id).public_ip_address)
+
+instances = createInstances(2)
+
+createInventory(instances, 'private.key')
+
+
 
 # createInventory('private.key')
